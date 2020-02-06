2f65b7be528ac1ba85ceceba326cacce9d69419c
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-02-06 14:07:10 -0500

    added Loop logic

--- GC_Lab_4.2/Program.cs
+++ GC_Lab_4.2/Program.cs
@@ -6,7 +6,34 @@ namespace GC_Lab_4._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Movie Merch");
+
+            do
+            {
+
+            } while (SearchAgainPrompt());
+        }
+
+        private static bool SearchAgainPrompt()
+        {
+            do
+            {
+                Console.Write("Do you want to search again?");
+                char key = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if ("Yy".Contains(key))
+                {
+                    return true;
+                }
+                else if ("Nn".Contains(key))
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("type y for yes or n for no");
+                }
+            } while (true);
         }
     }
 }
